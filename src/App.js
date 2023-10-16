85277ca7d088f386c19f280838704a2721e4e13f
MorganDourdoigne <dourdoigne.morgan@gmail.com>
2023-10-16 17:17:10 +0200

    mise en place components

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import React from "react";
 import Header from "./components/Header";
-import Main from "./components/Main";
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
+import SignInPage from "./pages/SignInPage";
+import UserProfilePage from "./pages/UserProfilePage";
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Main />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+          <Route path="/signin">
+            <SignInPage />
+          </Route>
+          <Route path="/profile">
+            <UserProfilePage />
+          </Route>
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,13 @@
-// zefgtrrezdsq
+import React from 'react';
+import '../index.css';
+
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p className="footer-text">Copyright 2020 Argent Bank</p>
+    </footer>
+  );
+}
+
+export default Footer;
