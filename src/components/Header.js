7a69561f0e036708e9f18a3a8c6b8582c9dd4caf
MorganDourdoigne <dourdoigne.morgan@gmail.com>
2023-10-17 14:44:30 +0200

    mise en place sass

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,27 @@
 import { Link } from "react-router-dom";
 import React from 'react';
 import logoArgentBank from '../img/argentBankLogo.png';
-import '../index.css';
 
 
 function Header() {
   return (
-    <nav className="main-nav">
-      <Link className="main-nav-logo" to="/">
-        <img className="main-nav-logo-image" src={logoArgentBank} alt="Argent Bank Logo" />
-        <h1 className="sr-only">Argent Bank</h1>
-      </Link>
-      <div>
-        <Link className="main-nav-item" to="/user">
-          <i className="fa fa-user-circle"></i>
-          Tony
+    <header>
+      <nav className="main-nav">
+        <Link className="main-nav-logo" to="/">
+          <img
+            className="main-nav-logo-image"
+            src={logoArgentBank}
+            alt="Argent Bank Logo"
+          />
+          <h1 className="sr-only">Argent Bank</h1>
         </Link>
-        <Link className="main-nav-item" to="/">
-          <i className="fa fa-sign-out"></i>
-          Sign Out
-        </Link>
-      </div>
-    </nav>
-  );
+        {/* <div>
+          <LogIn />
+          <LogOut />
+        </div> */}
+      </nav>
+    </header>
+  )
 }
 
 export default Header;
