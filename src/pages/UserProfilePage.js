41ef0443b7a80c984b9cec9ac49b5f9dc2708405
MorganDourdoigne <dourdoigne.morgan@gmail.com>
2023-10-26 10:15:11 +0200

    mise en place comp. account ok

--- src/pages/UserProfilePage.js
+++ src/pages/UserProfilePage.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import UpdateUsername from "../components/UpdateUsernameForm";
+import Account from "../components/Account";
 
 function UserProfilePage() {
   const dispatch = useDispatch();
@@ -46,7 +47,7 @@ function UserProfilePage() {
           <h1>
             Welcome back
             <br />
-            {data ? `${data.firstName} ${data.lastName}` : ''} !
+            {data ? `${data.firstName} ${data.lastName}` : ""} !
           </h1>
           <button
             className="edit-button"
@@ -56,10 +57,22 @@ function UserProfilePage() {
           </button>
         </div>
         {isEditing && <UpdateUsername setIsEditing={setIsEditing} />}
-        <h2 className="sr-only">Comptes</h2>
-        {/* {data.accounts.map((account) => (
-          <Account key={account.id} account={account} />
-        ))} */}
+        <h2 className="sr-only">Accounts</h2>
+        <Account
+          title="Argent Bank Checking (x8349)"
+          amount="$2,082.79"
+          description="Available Balance"
+        />
+        <Account
+          title="Argent Bank Savings (x6712)"
+          amount="$10,928.42"
+          description="Available Balance"
+        />
+        <Account
+          title="Argent Bank Credit Card (x8349)"
+          amount="$184.30"
+          description="Current Balance"
+        />
       </main>
     </div>
   );
